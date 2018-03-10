21ecb78cb26712ce814c3be445176a95ee72f2c5
Bonni Rambatan <bonni.rambatan@gmail.com>
2018-03-11 02:03:23 +0700

    Initial Commit of Angular Site
    Initial commit of the actual site. Landing page designed but not yet working.

---
+++ app/subscribe/subscribe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subscribe',
+  templateUrl: './subscribe.component.html',
+  styles: []
+})
+export class SubscribeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
